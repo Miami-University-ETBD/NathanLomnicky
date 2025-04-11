dd9a9c4967e98bbbacb26e247a0d67808a4be472
FoxfordOH <lomnicnj@miamioh.edu>
2025-04-11 18:03:44 -0400

    Created JQuery File and Created Page Header
    Added basic JQuery stuff and then adjusted other pages to include navigation to JQuery page

--- interactions.js
+++ interactions.js
@@ -1,18 +1,18 @@
 let phoneNum;
 let idea;
 
-document.getElementById("mySubmit").onclick = function(){
+document.getElementById("mySubmit").onclick = function () {
     phoneNum = document.getElementById("numberInput").value;
     console.log(phoneNum);
 }
 
-document.getElementById("ideaSubmit").onclick = function(){
+document.getElementById("ideaSubmit").onclick = function () {
     idea = document.getElementById("ideaInput").value;
     console.log(idea);
 }
 
- // Initialize modal
-document.addEventListener('DOMContentLoaded', function() {
+// Initialize modal
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
